2a85d0c724980dbbd8c0b3056a298ca069238c1b
Oktay Genc <gncoktay3866@gmail.com>
2024-10-27 21:38:29 +0300

    feat: Create NoteEntity class for database representation

--- app/src/main/java/com/oktaygenc/notmi/data/model/NoteEntity.kt
+++ app/src/main/java/com/oktaygenc/notmi/data/model/NoteEntity.kt
@@ -1,4 +1,12 @@
 package com.oktaygenc.notmi.data.model
 
-class NoteEntity {
-}
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class NoteEntity (
+    @PrimaryKey val id : Int,
+    @ColumnInfo(name = "title") val title : String?,
+    @ColumnInfo(name = "content") val content : String?
+)
