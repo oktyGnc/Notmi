3f6791f4659901c9f3c81dda183127321e16afd0
Oktay Genc <gncoktay3866@gmail.com>
2024-10-27 21:51:39 +0300

    feat: Implement NoteRepositoryImpl for data handling logic

--- app/src/main/java/com/oktaygenc/notmi/data/repository/NoteRepositoryImpl.kt
+++ app/src/main/java/com/oktaygenc/notmi/data/repository/NoteRepositoryImpl.kt
@@ -1,4 +1,27 @@
 package com.oktaygenc.notmi.data.repository
 
-class NoteRepositoryImpl {
+import com.oktaygenc.notmi.data.local.NoteDao
+import com.oktaygenc.notmi.data.model.NoteEntity
+
+class NoteRepositoryImpl(private val noteDao: NoteDao) {
+
+    suspend fun addNote(note: NoteEntity) {
+        noteDao.insert(note)
+    }
+
+    suspend fun getAllNotes(): List<NoteEntity> {
+        return noteDao.getAllNotes()
+    }
+
+    suspend fun deleteNote(note: NoteEntity) {
+        noteDao.delete(note)
+    }
+
+    suspend fun updateNote(note: NoteEntity) {
+        noteDao.update(note)
+    }
+
+    suspend fun getNoteById(id: Int): NoteEntity? {
+        return noteDao.getNoteById(id)
+    }
 }
