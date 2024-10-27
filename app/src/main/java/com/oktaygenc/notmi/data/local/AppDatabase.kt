3c2b92eb5c8e175780b48774e14a1b81737463fc
Oktay Genc <gncoktay3866@gmail.com>
2024-10-27 21:46:46 +0300

    feat: Create AppDatabase class for Room database setup

--- app/src/main/java/com/oktaygenc/notmi/data/local/AppDatabase.kt
+++ app/src/main/java/com/oktaygenc/notmi/data/local/AppDatabase.kt
@@ -1,4 +1,28 @@
 package com.oktaygenc.notmi.data.local
 
-class AppDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.oktaygenc.notmi.data.model.NoteEntity
+
+@Database(entities = [NoteEntity::class], version = 1, exportSchema = false)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun noteDao(): NoteDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = androidx.room.Room.databaseBuilder(
+                    context.applicationContext, AppDatabase::class.java, "note_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+
+
 }
