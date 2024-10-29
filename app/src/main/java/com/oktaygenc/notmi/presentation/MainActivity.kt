d938ec64a55a6731f3c8c26f12b1ffc10d0d642e
Oktay Genc <gncoktay3866@gmail.com>
2024-10-29 23:02:25 +0300

    feat: Add NoteViewModel with business logic for note management

--- app/src/main/java/com/oktaygenc/notmi/presentation/MainActivity.kt
+++ app/src/main/java/com/oktaygenc/notmi/presentation/MainActivity.kt
@@ -25,7 +25,7 @@ class MainActivity : AppCompatActivity() {
 
         /*
 
-        * 8. ViewModel OluşturmaNoteViewModel.kt Oluştur: LiveData ile kullanıcı arayüzüne veri sağlayacak ViewModel'i yaz.
+        * 8. ViewModel Oluşturma NoteViewModel.kt Oluştur: LiveData ile kullanıcı arayüzüne veri sağlayacak ViewModel'i yaz.
 
         * 9. UI GeliştirmeFragment ve Activity’leri Oluştur:AddNoteFragment.kt (not eklemek için)NoteListFragment.kt (not listesini göstermek için)
             NoteDetailFragment.kt (not detaylarını göstermek için NoteAdapter.kt Oluştur: Notların listeleneceği bir adapter oluştur.
--- app/src/main/java/com/oktaygenc/notmi/presentation/viewmodel/NoteViewModel.kt
+++ app/src/main/java/com/oktaygenc/notmi/presentation/viewmodel/NoteViewModel.kt
@@ -1,10 +1,63 @@
 package com.oktaygenc.notmi.presentation.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.oktaygenc.notmi.data.model.NoteEntity
 import com.oktaygenc.notmi.data.repository.NoteRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class NoteViewModel @Inject constructor(private val repository: NoteRepository) : ViewModel()  {
+class NoteViewModel @Inject constructor(private val repository: NoteRepository) : ViewModel() {
+
+    private val _notes = MutableLiveData<List<NoteEntity>>()
+    val notes: LiveData<List<NoteEntity>> get() = _notes
+
+    init {
+        getAllNotes()
+    }
+
+    private fun getAllNotes() {
+        viewModelScope.launch {
+            _notes.value = repository.getAllNotes()
+        }
+    }
+
+    fun addNote(note: NoteEntity) {
+        viewModelScope.launch {
+            repository.insertNote(note)
+            getAllNotes()
+        }
+    }
+
+    fun deleteNoteById(id: Int) {
+        viewModelScope.launch {
+            repository.deleteNoteById(id)
+            getAllNotes()
+        }
+    }
+
+
+    fun updateNote(note: NoteEntity) {
+        viewModelScope.launch {
+            repository.updateNote(note)
+            getAllNotes()
+        }
+    }
+
+    fun searchNotes(query: String) {
+        viewModelScope.launch {
+            val allNotes = repository.getAllNotes()
+            _notes.value = allNotes.filter { note ->
+                note.title?.contains(query, ignoreCase = true) == true || note.content?.contains(
+                    query,
+                    ignoreCase = true
+                ) == true
+            }
+        }
+    }
+
 }
