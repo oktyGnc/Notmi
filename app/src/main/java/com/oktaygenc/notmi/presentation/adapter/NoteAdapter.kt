661bbb97725c4475bbf3bc44ad3e48b535e7536c
Oktay Genc <gncoktay3866@gmail.com>
2024-10-29 23:23:39 +0300

    feat: Implement NoteAdapter for displaying notes in RecyclerView

--- app/src/main/java/com/oktaygenc/notmi/presentation/adapter/NoteAdapter.kt
+++ app/src/main/java/com/oktaygenc/notmi/presentation/adapter/NoteAdapter.kt
@@ -1,4 +1,34 @@
 package com.oktaygenc.notmi.presentation.adapter
 
-class NoteAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.oktaygenc.notmi.data.model.NoteEntity
+import com.oktaygenc.notmi.databinding.ItemViewNoteBinding
+
+class NoteAdapter (private var notes: List<NoteEntity>) : RecyclerView.Adapter<NoteAdapter.NoteViewHolder>() {
+
+    inner class NoteViewHolder(private val binding: ItemViewNoteBinding):RecyclerView.ViewHolder(binding.root){
+        fun bind(note:NoteEntity){
+            binding.textViewTitle.text = note.title
+            binding.textViewContent.text = note.content
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+        val binding = ItemViewNoteBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return NoteViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int = notes.size
+
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        holder.bind(notes[position])
+    }
+
+    fun updateNotes(newNotes: List<NoteEntity>) {
+        notes = newNotes
+        notifyDataSetChanged()
+    }
 }
