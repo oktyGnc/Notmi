176121d1d597ab396c524ce5489f04172af37386
Oktay Genc <gncoktay3866@gmail.com>
2024-10-31 00:04:13 +0300

    feat: Added toolbar to the project

--- app/src/main/java/com/oktaygenc/notmi/presentation/MainActivity.kt
+++ app/src/main/java/com/oktaygenc/notmi/presentation/MainActivity.kt
@@ -1,29 +1,33 @@
 package com.oktaygenc.notmi.presentation
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.oktaygenc.notmi.R
 import com.oktaygenc.notmi.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
-        /*
+        val toolbar = binding.toolbar
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = "Notlarım"
+
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+    }/*
         * 9. UI GeliştirmeFragment ve Activity’leri Oluştur:AddNoteFragment.kt (not eklemek için)NoteListFragment.kt (not listesini göstermek için)
             NoteDetailFragment.kt (not detaylarını göstermek için NoteAdapter.kt Oluştur: Notların listeleneceği bir adapter oluştur.
             *
@@ -31,5 +35,4 @@ class MainActivity : AppCompatActivity() {
 
         * 11. CRUD İşlemleri UygulaNot Ekleme, Silme, Düzeltme ve Arama Fonksiyonlarını Uygula.
          */
-    }
 }
--- app/src/main/java/com/oktaygenc/notmi/presentation/ui/NoteListFragment.kt
+++ app/src/main/java/com/oktaygenc/notmi/presentation/ui/NoteListFragment.kt
@@ -6,7 +6,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.oktaygenc.notmi.data.model.NoteEntity
 import com.oktaygenc.notmi.databinding.FragmentNoteListBinding
+import com.oktaygenc.notmi.presentation.adapter.NoteAdapter
 import com.oktaygenc.notmi.presentation.viewmodel.NoteViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -25,6 +28,62 @@ class NoteListFragment : Fragment() {
         val view = binding.root
         return view
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupRecyclerView()
+    }
+
+    private fun setupRecyclerView() {
+        val notes = listOf(
+            NoteEntity(id = 1, title = "Grocery List", content = "Eggs, Milk, Bread, Butter"),
+            NoteEntity(
+                id = 2,
+                title = "Meeting Notes",
+                content = "Discuss project timeline and milestones."
+            ),
+            NoteEntity(
+                id = 3,
+                title = "Book Recommendations",
+                content = "1. The Great Gatsby\n2. To Kill a Mockingbird\n3. 1984"
+            ),
+            NoteEntity(
+                id = 4,
+                title = "Travel Plans",
+                content = "Visit Paris in June. Book flights and hotel."
+            ),
+            NoteEntity(id = 5, title = "Grocery List", content = "Eggs, Milk, Bread, Butter"),
+            NoteEntity(
+                id = 6,
+                title = "Meeting Notes",
+                content = "Discuss project timeline and milestones."
+            ),
+            NoteEntity(
+                id = 7,
+                title = "Book Recommendations",
+                content = "1. The Great Gatsby\n2. To Kill a Mockingbird\n3. 1984"
+            ),
+            NoteEntity(
+                id = 8,
+                title = "Travel Plans",
+                content = "Visit Paris in June. Book flights and hotel."
+            )
+        )
+
+        val notesAdapter = NoteAdapter(notes)
+
+        binding.recyclerView.apply {
+            layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+            adapter = notesAdapter
+        }
+
+
+    }
+
+//        binding.recyclerViewNotes.apply {
+//            layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+//            adapter = notesAdapter
+//        }
 
 
     override fun onDestroyView() {
