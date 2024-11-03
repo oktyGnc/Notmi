9269b97497ff7b8225fd9b26ccb407c0ea230564
Oktay Genc <gncoktay3866@gmail.com>
2024-11-03 03:17:40 +0300

    feat: Add and enable splash screen for initial app launch experience

---
+++ app/src/main/java/com/oktaygenc/notmi/presentation/ui/splashscreen/SplashScreen.kt
@@ -0,0 +1,21 @@
+package com.oktaygenc.notmi.presentation.ui.splashscreen
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.oktaygenc.notmi.R
+import com.oktaygenc.notmi.presentation.MainActivity
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 2000)
+    }
+}
